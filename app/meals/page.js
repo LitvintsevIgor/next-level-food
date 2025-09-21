aef4b779a04672c88350a05ef79684335ca72fa4
Igor Litvintsev <LitvintsevII@msk.bcs.ru>
2025-09-21 23:13:39 +0300

    add Suspense

--- app/meals/page.js
+++ app/meals/page.js
@@ -3,10 +3,14 @@ import Link from "next/link";
 
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function Meals() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +24,11 @@ export default async function Meals() {
         </p>
       </header>
       <main>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
