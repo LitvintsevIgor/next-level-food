c28690a81af0c005f9f33a7983c7bf5616445602
Igor Litvintsev <LitvintsevII@msk.bcs.ru>
2025-09-23 00:00:00 +0300

    add MealDetails

--- app/meals/not-found.js
+++ app/meals/not-found.js
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <main className="not-found">
-      <h1>Not found</h1>
+      <h1>Meal not found</h1>
       <p>Unfortunately, the page you are looking for was not found.</p>
     </main>
   );
